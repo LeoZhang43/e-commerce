7a660698c5ead845b47a79f2f64e8ca121f419f7
LeoZhang43 <yzhang43@usc.edu>
2023-07-12 13:32:17 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components'
+import { Home, SingleProduct, Cart, Checkout, Error, About, Products, PrivateRoute } from './pages'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/products/:id' element={<SingleProduct />}></Route>
+        <Route path='/checkout' element={<Checkout />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
